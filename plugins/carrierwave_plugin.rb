b279ff2796c048abc9e482a5e8a89d79299bb7d0
xavierRiley <xavier@kyanmedia.com>
2011-08-31 11:56:45 +0200

    More complete carrierwave recipie for Padrino incorporating better file handling based on extensions from this gist https://gist.github.com/995663 Creates folders and permissions, specifies necessary gems.

--- plugins/carrierwave_plugin.rb
+++ plugins/carrierwave_plugin.rb
@@ -11,7 +11,12 @@ class Uploader < CarrierWave::Uploader::Base
   #
   # include CarrierWave::RMagick
   # include CarrierWave::ImageScience
-  # include CarrierWave::MiniMagick
+  include CarrierWave::MiniMagick
+  
+  IMAGE_EXTENSIONS = %w(jpg jpeg gif png)
+  DOCUMENT_EXTENSIONS = %w(exe pdf doc docm xls)
+  AUDIO_EXTENSIONS = %w(mp3 wma ogg wav)
+  VIDEO_EXTENSIONS = %w(mp4 mov)
 
   ##
   # Storage type
@@ -35,9 +40,9 @@ class Uploader < CarrierWave::Uploader::Base
   ##
   # Directory where uploaded files will be stored (default is /public/uploads)
   #
-  def store_dir
-    'images/uploads'
-  end
+  #def store_dir
+  #  'images/uploads'
+  #end
 
   ##
   # Directory where uploaded temp files will be stored (default is [root]/tmp)
@@ -71,8 +76,46 @@ class Uploader < CarrierWave::Uploader::Base
   # White list of extensions which are allowed to be uploaded:
   #
   def extension_white_list
-    %w(jpg jpeg gif png)
+    IMAGE_EXTENSIONS + DOCUMENT_EXTENSIONS + AUDIO_EXTENSIONS + VIDEO_EXTENSIONS
   end
+  
+  # create a new "process_extensions" method. It is like "process", except
+  # it takes an array of extensions as the first parameter, and registers
+  # a trampoline method which checks the extension before invocation
+  def self.process_extensions(*args)
+    extensions = args.shift
+    args.each do |arg|
+      if arg.is_a?(Hash)
+        arg.each do |method, args|
+          processors.push([:process_trampoline, [extensions, method, args]])
+        end
+      else
+        processors.push([:process_trampoline, [extensions, arg, []]])
+      end
+    end
+  end
+
+  # our trampoline method which only performs processing if the extension matches
+  def process_trampoline(extensions, method, args)
+    extension = File.extname(original_filename).downcase
+    extension = extension[1..-1] if extension[0,1] == '.'
+    self.send(method, *args) if extensions.include?(extension)
+  end
+  
+  # version actually defines a class method with the given block
+  # therefore this code does not run in the context of an object instance
+  # and we cannot access uploader instance fields from this block
+  version :admin_thumb do
+    process_extensions Uploader::IMAGE_EXTENSIONS, :resize_to_fit => [80,80]
+  end
+
+  #version :logo do
+  #  process_extensions Uploader::IMAGE_EXTENSIONS, :resize_to_fit => [210,100]
+  #end
+  
+  #version :thumb do
+  #  process_extensions ImageUploader::IMAGE_EXTENSIONS, :resize_to_fill => [35, 35]
+  #end
 
   ##
   # Override the filename of the uploaded files
@@ -82,6 +125,14 @@ class Uploader < CarrierWave::Uploader::Base
   # end
 end
 UPLOADER
+inject_into_file destination_root('Gemfile'),"gem 'mini_magick'\n", :after => "# Component requirements\n"
+inject_into_file destination_root('Gemfile'),"gem 'carrierwave'\n", :after => "# Component requirements\n"
+empty_directory destination_root('public/uploads')
+empty_directory destination_root('public/uploads/images')
+empty_directory destination_root('public/uploads/documents')
+empty_directory destination_root('public/uploads/audio')
+empty_directory destination_root('public/uploads/video')
+chmod destination_root('public/uploads/*'), 777
 create_file destination_root('lib/uploader.rb'), UPLOADER
 generate :model, "upload file:text created_at:datetime"
 prepend_file destination_root('models/upload.rb'), "require 'carrierwave/orm/#{fetch_component_choice(:orm)}'\n"
